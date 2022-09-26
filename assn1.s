525c3d2e033cb30a227d1ba85dcfe9a34b9b5be4
Mehar Khurana <meharkhurana03@gmail.com>
2022-09-26 23:12:59 +0530

    first commit

---
+++ assn1.s
@@ -0,0 +1,21 @@
+extern printf
+
+SECTION .DATA
+    msg:        db "Hello World", 0
+    fmt:        db "%s", 10, 0
+
+SECTION .TEXT
+    global main
+
+main:
+    push rbp
+
+    mov rdi, fmt
+    mov rsi, msg
+    mov rax, 0
+    call printf
+
+    pop rbp
+    mov rax, 0
+    ret
+
