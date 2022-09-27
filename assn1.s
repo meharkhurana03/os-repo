3cd3d8e800bda0a1237cb011dd71f2adbce04d95
Mehar Khurana <meharkhurana03@gmail.com>
2022-09-27 16:31:19 +0530

    changed assn1.s

--- assn1.s
+++ assn1.s
@@ -1,22 +1,52 @@
-extern printf
+extern printf, scanf
 
-SECTION .DATA
+section .data
     msg:        db "Hello World", 0
-    fmt:        db "%s", 10, 0
+    fmt:        db "Enter a string: %s", 0
+    s:          times 128 db 0 ; creates a string of 128-long null characters
+    fmt2:       db "Enter a number: %d", 0
+    fmtout:     db "Your number is %d", 0
+    fmtout2:    db "Your entered: %s", 0
 
-SECTION .TEXT
-    global main
+
+section .text
+    global  main
 
 main:
-    push rbp
+    push    rbp
+    mov     rbp, rsp
+    sub     rsp, 4
 
-    ; setting up args for printf
-    mov rdi, fmt
-    mov rsi, msg
-    mov rax, 0
-    call printf
+    mov     rdi, msg
+    xor     rax, rax
+    call    printf
+    
+    ; setting up args for scanning int
+    mov     rdi, fmt2   ; lea
+    mov     rsi, [rbp-4]
+    xor     rax, rax ; rax zeroed out for return value
+    call    scanf
 
-    pop rbp
-    mov rax, 0
+    ; setting up args for scanning string
+    mov     rdi, fmt
+    mov     rsi, s
+    xor     rax, rax
+    call    scanf
+    
+    ; setting up args for printing int
+    mov     rdi, fmtout
+    mov     rsi, [rbp-4]
+    xor     rax, rax
+    call    printf
+
+    ; setting up args for printing string
+    mov     rdi, fmtout2
+    mov     rsi, s
+    xor     rax, rax
+    call    printf
+
+    xor     rax, rax ; rax zeroed out for return value
+    mov     rsp, rbp
+    pop     rbp
     ret
 
