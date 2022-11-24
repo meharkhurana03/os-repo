5dbe2ec78ecc5b8f0ae90b4dd795bb53debf67bf
Mehar Khurana <meharkhurana03@gmail.com>
2022-11-24 19:40:46 +0530

    added make nconfig to scripts

--- Assn2/script2.sh
+++ Assn2/script2.sh
@@ -1,3 +1,4 @@
 cd /root/kernelbuild-2/kernelbuild-1/linux-5.15.79
 wget -P /root/kernelbuild-2/kernelbuild-1/linux-5.15.79 --no-check-certificate 'https://docs.google.com/uc?export=download&id=1YvJeK9Wfz-1PStKuElGP_0LCEr4zoulU' -O .config
+make nconfig
 make -j3
